7aed8e97bea5be789fe37164eb22ecb3bb16b31c
sbrycbc <sabriyecbc@gmail.com>
2023-05-16 13:06:34 +0200

    updated movies.forEach..

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const actors_a = document.querySelectorAll(".actors_a");
 
 movies.forEach(function(aitem) {
     aitem.addEventListener("click", function(){
+        movies.forEach(function(aitem){
         aitem.classList.remove("active");
+        })
+        actors_a.forEach(function(actors_item){
+            actors_item.classList.remove("active");
+        })
+        aitem.classList.add("active");
     })
 })
